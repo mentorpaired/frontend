09f91555f1558705c8ac923a3577779911f17fac
ailevi <nkoliokafor@gmail.com>
2020-09-28 22:36:21 +0100

    Add about reducer to list of exported reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import githubAuth from "./githubAuth";
+import aboutReducer from "./aboutReducer";
 
 export default combineReducers({
   githubAuth,
+  aboutReducer,
 });
