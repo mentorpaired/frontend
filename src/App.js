e83f7c03f230d99046e4832af7910f951c0a723b
ailevi <nkoliokafor@gmail.com>
2020-09-28 22:25:30 +0100

    Switch to class based component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -10,21 +10,23 @@ import UserInformation from "./pages/userinformation/UserInformation";
 import Profile from "./pages/profile/Profile";
 import PrivateRoute from "./routes/PrivateRoute";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path="/" component={Dashboard} />
-          <Route path="/about" component={About} />
-          <Route path="/user-info" component={UserInformation} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <Route path="/about" component={About} />
+            <Route path="/user-info" component={UserInformation} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
