bcb79f9756bcf36b2893dfb1622ab4c5edcbf7ba
ailevi <nkoliokafor@gmail.com>
2020-09-29 18:27:41 +0100

    Map user data to appropriate tags

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -11,49 +11,43 @@ class About extends Component {
   }
   render() {
     return (
-      // {this.props.user.map(u => {
-      <div className="about_container">
-        <div className="about_appname">MentorPaired</div>
+      <>
+        {this.props.user.map((u) => {
+          <div className="about_container">
+            <div className="about_appname">MentorPaired</div>
 
-        <div className="image_name_location_container">
-          <div className="image">
-            <FontAwesomeIcon icon={faCamera} size="2x" className="fa_camera" />
-            <img src="#" alt="user_avatar" />
-          </div>
+            <div className="image_name_location_container">
+              <div className="image">
+                <FontAwesomeIcon
+                  icon={faCamera}
+                  size="2x"
+                  className="fa_camera"
+                />
+                <img src="#" alt="user_avatar" />
+              </div>
 
-          <div className="name_location">
-            <p className="name">Delores Diei</p>
-            <div className="location">
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                size="2x"
-                className="fa_location_icon"
-              />
-              <p>Munich, Germany</p>
+              <div className="name_location">
+                <p className="name">{u.display_name}</p>
+                <div className="location">
+                  <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    size="2x"
+                    className="fa_location_icon"
+                  />
+                  <p>{u.timezone}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="bio_container">
-          <p className="name">About Name</p>
-          <div className="description">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              nisl nunc, mollis in urna vel, tincidunt congue arcu. Pellentesque
-              et felis mauris. Suspendisse non lectus viverra, hendrerit ex
-              lobortis, suscipit mi. Pellentesque habitant morbi tristique
-              senectus et netus et malesuada fames ac turpis egestas. Nulla vel
-              aliquet lectus, sit amet efficitur dolor. Maecenas aliquet libero
-              in massa convallis feugiat eu vitae neque. Maecenas nec lacus
-              efficitur, eleifend erat ac, consectetur nisl. Aliquam nisl erat,
-              ultricies nec enim nec, porta gravida felis. Nulla facilisi.
-              Praesent blandit justo ex, sit amet ultricies ipsum auctor
-              ultricies. Sed tristique tristique nibh in{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-      // })}
+            <div className="bio_container">
+              <p className="name">About {u.display_name}</p>
+              <div className="description">
+                <p>{u.about}</p>
+              </div>
+            </div>
+          </div>;
+        })}
+      </>
     );
   }
 }
