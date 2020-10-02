60aded9a19e2cc498af848a1721c2a7af13fd511
ailevi <nkoliokafor@gmail.com>
2020-10-02 17:02:02 +0100

    Create action for retrieving user information based on the user's id

--- src/actions/aboutAction.js
+++ src/actions/aboutAction.js
@@ -1,28 +1,40 @@
-import { LOAD_USER_INFO, LOAD_USER_INFO_ERROR } from "./actionTypes";
+import axios from "axios";
+import {
+  RETRIEVING_USER_INFO,
+  RETRIEVE_USER_INFO_ERROR,
+  RETRIEVED_USER_INFO,
+} from "./actionTypes";
 
 const url = process.env.REACT_APP_BACKEND_PROXY_URL;
 
-// Retrieve user information
-export const loadUser = (id) => {
-  return (dispatch) => {
-    dispatch({ type: LOAD_USER_INFO });
-    try {
-      let token = localStorage.getItem("token");
-      fetch(`${url}/users/${id}/`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          localStorage.setItem("user", JSON.stringify(data.user));
-        });
-    } catch (err) {
-      return dispatch({
-        type: LOAD_USER_INFO_ERROR,
-      });
-    }
+// Retrieve user information from jwt access token
+export const getUserInfo = (user_id) => (dispatch, getState) => {
+  dispatch({ type: RETRIEVING_USER_INFO });
+
+  // get access token
+  const jwt = getState().githubAuth.jwt.access;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
+
+  // if jwt, add to headers config
+  if (jwt) {
+    config.headers["Authorization"] = `Bearer ${jwt}`;
+  }
+
+  axios
+    .get(`${url}/users/${user_id}`, config)
+    .then((res) => {
+      dispatch({
+        type: RETRIEVED_USER_INFO,
+        payload: res.data,
+      });
+    })
+    .catch({
+      type: RETRIEVE_USER_INFO_ERROR,
+    });
 };
