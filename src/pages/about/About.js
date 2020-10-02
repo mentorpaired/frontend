8033bbf08eb1b9a78fdb4b34a060ca51e6e24d4a
ailevi <nkoliokafor@gmail.com>
2020-10-02 17:05:52 +0100

    Map user's data to appropriate tags

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,51 +1,51 @@
 import React, { Component } from "react";
 import "./About.css";
 import { connect } from "react-redux";
-import { loadUser } from "../../actions/aboutAction";
+import { getUserInfo } from "../../actions/aboutAction";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 class About extends Component {
   componentDidMount() {
-    this.props.loadUser();
+    this.props.getUserInfo();
   }
   render() {
     return (
       <>
-        {/* {this.props.user.map((u) => ( */}
-        <div className="about_container">
-          <div className="about_appname">MentorPaired</div>
+        {this.props.user.map((u) => (
+          <div className="about_container" key={u.user_id}>
+            <div className="about_appname">MentorPaired</div>
 
-          <div className="image_name_location_container">
-            <div className="image">
-              <FontAwesomeIcon
-                icon={faCamera}
-                size="2x"
-                className="fa_camera"
-              />
-              <img src="#" alt="user_avatar" />
+            <div className="image_name_location_container">
+              <div className="image">
+                <FontAwesomeIcon
+                  icon={faCamera}
+                  size="2x"
+                  className="fa_camera"
+                />
+                <img src={u.avatar} alt="user_avatar" />
+              </div>
+
+              <div className="name_location">
+                <p className="name">{u.display_name}</p>
+                <div className="location">
+                  <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    size="2x"
+                    className="fa_location_icon"
+                  />
+                  <p>{u.timezone}</p>
+                </div>
+              </div>
             </div>
 
-            <div className="name_location">
-              <p className="name">Display Name</p>
-              <div className="location">
-                <FontAwesomeIcon
-                  icon={faMapMarkerAlt}
-                  size="2x"
-                  className="fa_location_icon"
-                />
-                <p>Location</p>
+            <div className="bio_container">
+              <p className="name">About {u.display_name}</p>
+              <div className="description">
+                <p>{u.about}</p>
               </div>
             </div>
           </div>
-
-          <div className="bio_container">
-            <p className="name">About Display Name</p>
-            <div className="description">
-              <p>Bio</p>
-            </div>
-          </div>
-        </div>
         ))}
       </>
     );
@@ -56,4 +56,4 @@ const mapStateToProps = (state) => ({
   user: state.aboutReducer.user,
 });
 
-export default connect(mapStateToProps, { loadUser })(About);
+export default connect(mapStateToProps, { getUserInfo })(About);
