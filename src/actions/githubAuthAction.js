dd8019cc6b2dd3f109a10aa94c95daa0b69483bc
ailevi <nkoliokafor@gmail.com>
2020-10-05 21:34:07 +0100

    remove try/catch, add return statement in github action

--- src/actions/githubAuthAction.js
+++ src/actions/githubAuthAction.js
@@ -10,42 +10,41 @@ const url = process.env.REACT_APP_BACKEND_PROXY_URL;
 const githubAuthAction = (code) => {
   return (dispatch) => {
     dispatch({ type: LOADING });
-    try {
-      fetch(`${url}/github_auth/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          localStorage.setItem("isLoggedIn", true);
-          localStorage.setItem("token", JSON.stringify(data.token));
-          localStorage.setItem("jwt", JSON.stringify(data.jwt));
-          localStorage.setItem("user", JSON.stringify(data.user));
-          dispatch({
-            type: AUTHENTICATED,
-            payload: {
-              isLoading: false,
-              isAuthenticated: true,
-              token: data.token,
-            },
-          });
-          dispatch({
-            type: GITHUB_LOGIN_SUCCESS,
-            payload: {
-              isLoggedIn: true,
-              jwt: data.jwt,
-              user: data.user,
-            },
-          });
+    return fetch(`${url}/github_auth/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("token", JSON.stringify(data.token));
+        localStorage.setItem("jwt", JSON.stringify(data.jwt));
+        localStorage.setItem("user", JSON.stringify(data.user));
+        dispatch({
+          type: AUTHENTICATED,
+          payload: {
+            isLoading: false,
+            isAuthenticated: true,
+            token: data.token,
+          },
         });
-    } catch (error) {
-      return dispatch({
-        type: GITHUB_LOGIN_FAIL,
+        dispatch({
+          type: GITHUB_LOGIN_SUCCESS,
+          payload: {
+            isLoggedIn: true,
+            jwt: data.jwt,
+            user: data.user,
+          },
+        });
+      })
+      .catch((error) => {
+        return dispatch({
+          type: GITHUB_LOGIN_FAIL,
+        });
       });
-    }
   };
 };
 
