a2356ff48501ea38d9e19db367cfd03807343271
Raul Luis <soyrauluis@outlook.com>
2020-11-19 16:22:53 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
+import Test from "./Test";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>~Hola mundo!</h1>
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/login" component={Test} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
