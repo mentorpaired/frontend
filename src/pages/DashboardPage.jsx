3612b1f67e5a09a9847c122088e441cb0bc78be6
Fola <foladee15@gmail.com>
2021-01-19 20:04:48 +0100

    implemented changes to sign_in page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,1 +1,27 @@
-import React from 'react';import { Route, Switch } from "react-router-dom";import Dashboard from '../modules/dashboard/components/Dashboard';import Messages from '../modules/dashboard/components/Messages';import Navbar from '../modules/dashboard/components/navbar/Navbar';import Profile from '../modules/dashboard/components/Profile';import Sidebar from '../modules/dashboard/components/sidebar/Sidebar';const DashboardPage = () => {  return (    <div className="main-wrapper">      <Sidebar/>      <div className="page-wrapper">        <Navbar/>        <div className="page-content">          <Switch>            <Route exact path="/" component={Dashboard}/>            <Route path="/profile" component={Profile}/>            <Route path="/messages" component={Messages}/>            <Route path="/signin" component={SignIn} />          </Switch>        </div>      </div>    </div>  );}export default DashboardPage;
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Dashboard from '../modules/dashboard/components/Dashboard';
+import Messages from '../modules/dashboard/components/Messages';
+import Navbar from '../modules/dashboard/components/navbar/Navbar';
+import Profile from '../modules/dashboard/components/Profile';
+import Sidebar from '../modules/dashboard/components/sidebar/Sidebar';
+
+const DashboardPage = () => {
+  return (
+    <div className="main-wrapper">
+      <Sidebar/>
+      <div className="page-wrapper">
+        <Navbar/>
+        <div className="page-content">
+          <Switch>
+            <Route exact path="/" component={Dashboard}/>
+            <Route path="/profile" component={Profile}/>
+            <Route path="/messages" component={Messages}/>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPage;
