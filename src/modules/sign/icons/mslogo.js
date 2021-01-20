1337a0a2695908b446cc42939e43bf3e64023325
Fola <foladee15@gmail.com>
2021-01-21 00:30:37 +0100

    fixed css and updated alt attribute

--- src/modules/sign/icons/mslogo.js
+++ src/modules/sign/icons/mslogo.js
@@ -1,1 +1,8 @@
-import React from 'react';import msLogo from '../../../assets/svg/microsoft.svg';const MicrosoftLogo = ({ className }) => {  return <img src={msLogo} alt="Microsoft Logo" className={className}/>}export default MicrosoftLogo;
+import React from 'react';
+import msLogo from '../../../assets/svg/microsoft.svg';
+
+const MicrosoftLogo = ({ className }) => {
+  return <img src={msLogo} alt="Microsoft Logo" className={className}/>
+}
+
+export default MicrosoftLogo;
