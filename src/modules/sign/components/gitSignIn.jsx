dcae55960853c2d198dadf6e15a5cd777f6d64fa
Fola <foladee15@gmail.com>
2021-01-31 13:10:17 +0100

    adjust button css

--- src/modules/sign/components/gitSignIn.jsx
+++ src/modules/sign/components/gitSignIn.jsx
@@ -7,20 +7,19 @@ import activateGitlab from "../../../../src/assets/svg/gitlabActive.svg";
 function GitSignIn() {
   const [githubActive, githubSetActive] = useState(false);
   const [gitlabActive, gitlabSetActive] = useState(false);
-  const githubActiveStyles = githubActive ? {border: "1px solid black", cursor: "pointer"} : {};
-  const gitlabActiveStyles = gitlabActive ? {border: "1px solid black", cursor: "pointer"} : {};
   const mouseOverGithub = () => githubSetActive(true);
   const mouseOverGitlab = () => gitlabSetActive(true);
   const mouseOutGithub = () => githubSetActive(false);
   const mouseOutGitlab = () => gitlabSetActive(false);
+
   return (
     <div className="git">
-      <button style={githubActiveStyles} className="github-button" onMouseOver={mouseOverGithub} onMouseOut={mouseOutGithub}>
+      <button className="github-button" onMouseOver={mouseOverGithub} onMouseOut={mouseOutGithub}>
         <img src={githubActive ? activateGithub : github} alt="github" />
         <span className="button-text">Continue with GitHub</span>
       </button>
       <br/>
-      <button style={gitlabActiveStyles} className="gitlab-button" onMouseOver={mouseOverGitlab} onMouseOut={mouseOutGitlab}>
+      <button className="gitlab-button" onMouseOver={mouseOverGitlab} onMouseOut={mouseOutGitlab}>
         <img src={gitlabActive ? activateGitlab : gitlab} alt="gitlab" />
         <span className="button-text">Continue with Gitlab</span>
       </button>
