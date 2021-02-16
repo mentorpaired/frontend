65cd8d5d501eddbcd1b046310fd0cc1bdcb2ac5d
Raul Luis <soyrauluis@outlook.com>
2021-02-15 23:52:59 -0400

    Split styles in different files for easier maintainability

--- src/modules/profile/components/Profile.jsx
+++ src/modules/profile/components/Profile.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import MentorButton from "./MentorButton";
-import "./profile.styles.scss";
-import UserDescription from "./UserDescription";
-import UserInfo from "./UserInfo";
+import React from 'react';
+import MentorButton from './MentorButton';
+import './profile.scss';
+import UserDescription from './UserDescription';
+import UserInfo from './UserInfo';
 
 export default function Profile({ user }) {
-  console.log(user);
-  return (
-    <div className="profile-container">
-      <UserInfo user={user} />
-      <UserDescription user={user} />
-    </div>
-  );
+	console.log(user);
+	return (
+		<div className='profile-container'>
+			<UserInfo user={user} />
+			<UserDescription user={user} />
+		</div>
+	);
 }
--- src/modules/profile/components/UserDescription.jsx
+++ src/modules/profile/components/UserDescription.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import AboutMe from "./AboutMe";
-import Goals from "./Goals";
-import Rating from "./Rating";
-import Reviews from "./Reviews";
+import React from 'react';
+import AboutMe from './AboutMe';
+import Goals from './Goals';
+import Rating from './Rating';
+import Reviews from './Reviews';
+import './user_description.scss';
 
 function UserDescription({ user }) {
-  console.log(user);
-  return (
-    <div className="user-description">
-      <AboutMe position={user.position} aboutme={user.aboutme} />
-      <Goals goals={user.goals} />
-      <Reviews reviews={user.reviews}>
-        <Rating
-          rating={user.rating}
-          section={"user-description"}
-          votes={user.votes}
-        />
-      </Reviews>
-    </div>
-  );
+	console.log(user);
+	return (
+		<div className='user-description'>
+			<AboutMe position={user.position} aboutme={user.aboutme} />
+			<Goals goals={user.goals} />
+			<Reviews reviews={user.reviews}>
+				<Rating
+					rating={user.rating}
+					section={'user-description'}
+					votes={user.votes}
+				/>
+			</Reviews>
+		</div>
+	);
 }
 
 export default UserDescription;
