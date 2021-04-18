4d1eb723f2a14b84d21f86122ae06b381f532f33
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2021-04-18 02:36:41 +0100

    styles: added the modal component

--- src/modules/dashboard/components/sidebar/Sidebar.jsx
+++ src/modules/dashboard/components/sidebar/Sidebar.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.styles.scss';
 import { Link, withRouter } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../../../assets/svg/logo.svg';
 import DashboardIcon from '../sidebar-icons/DashboardIcon';
 import ProfileIcon from '../sidebar-icons/ProfileIcon';
 import MessageIcon from '../sidebar-icons/MessageIcon';
+import Modal from '../modal/Modal';
 
 const Sidebar = (props) => {
+	const [visible, setVisible] = useState(false)
+	const makeModalVisible = () => {
+		setVisible(true)
+	}
+	const hideModal = () => {
+		setVisible(false)
+	}
 	return (
 		<div className='sidebar'>
+			{ visible ? (<Modal hideModal={hideModal}/>) : null}
 			<div className='sidebar-header'>
 				<div className='sidebar-logo'>
 					<Link to='#' className='sidebar-brand'>
@@ -48,7 +57,7 @@ const Sidebar = (props) => {
 						</Link>
 					</li>
 				</ul>
-				<button className='btn-req'>Request a mentor</button>
+				<button onClick={makeModalVisible} className='btn-req'>Request a mentor</button>
 				</div>
 			</div>
 		</div>
